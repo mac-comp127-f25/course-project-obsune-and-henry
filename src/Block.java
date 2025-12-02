58eb73abbeaa07b28a5dd43b5a1234119a57d830
ousso-png <ousso@macalester.edu>
2025-12-01 23:16:45 -0600

    Added stuff

--- src/Block.java
+++ src/Block.java
@@ -1,19 +1,22 @@
-import java.awt.Color;
 import edu.macalester.graphics.*;
 
 public class Block {
 
     private Rectangle block;
+    private Image image;
 
-    public Block(Point location){
+
+    public Block(double x, double y){
 
         Point size = new Point(140, 140);
 
-        this.block = new Rectangle(location, size);
-        this.block.setFillColor(Color.GRAY);
+        // this.block = new Rectangle(location, size);
+        // this.block.setFillColor(Color.GRAY);
+        // alicia = new Rectangle(location, size);
+        this.image = new Image(x, y, "AbbyMarsh.jpeg");
     }
 
-    public Rectangle getBlock(){
-        return this.block;
+    public Image getBlock(){
+        return this.image;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -14,18 +14,41 @@ public class Game {
     Point zeroone = new Point(205, 55);
     Point zerotwo = new Point(355, 55);
     Point zerothree = new Point(505, 55);
-
+    Point onezero = new Point(55, 205);
+    Point oneone = new Point(205, 205);
+    Point onetwo = new Point(355, 205);
+    Point onethree = new Point(505, 205);
+    Point twozero = new Point(55, 355);
+    Point twoone = new Point(205, 355);
+    Point twotwo = new Point(355, 355);
+    Point twothree = new Point(505, 355);
+    Point threezero = new Point(55, 505);
+    Point threeone = new Point(205, 505);
+    Point threetwo = new Point(355, 505);
+    Point threethree = new Point(505, 505);
     points.add(zerozero);
     points.add(zeroone);
     points.add(zerotwo);
     points.add(zerothree);
-
+    points.add(onezero);
+    points.add(oneone);
+    points.add(onetwo);
+    points.add(onethree);
+    points.add(twozero);
+    points.add(twoone);
+    points.add(twotwo);
+    points.add(twothree);
+    points.add(threezero);
+    points.add(threeone);
+    points.add(threetwo);
+    points.add(threethree);
+        
 
         CanvasWindow canvas = new CanvasWindow("2048 (MSCS Department Edition)", 700,700);
         GameBoard board = new GameBoard();
         canvas.add(board.createGrid());
 
-        Block block = new Block(zerothree);
+        Block block = new Block(threeone.getX(),threeone.getY());
         canvas.add(block.getBlock());
         canvas.draw();
     }
